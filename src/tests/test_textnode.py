c1c67dcf70522f681d6f654cc6fdd386bbfe45e8
BlochLior <blochlior@gmail.com>
2025-03-03 09:01:17 +0200

    repeated project assisted by the helpfiles, seems good now - copy_static entered to the main.py, added copystatic.py

---
+++ src/tests/test_textnode.py
@@ -0,0 +1,105 @@
+import unittest
+
+from src.textnode import TextNode, TextType, text_node_to_html_node
+from src.htmlnode import LeafNode, HTMLNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_uneq_1(self):
+        node1 = TextNode("placeholder1", TextType.TEXT)
+        node2 = TextNode("placeholder2", TextType.TEXT)
+        self.assertNotEqual(node1, node2)
+
+    def test_uneq_2(self):
+        node1 = TextNode("placeholder", TextType.CODE)
+        node2 = TextNode("placeholder", TextType.TEXT)
+        self.assertNotEqual(node1, node2)
+
+    def test_uneq_3(self):
+        node1 = TextNode("placeholder", TextType.TEXT, "url")
+        node2 = TextNode("placeholder", TextType.TEXT)
+        self.assertNotEqual(node1, node2)
+
+    def test_uneq_4(self):
+        node1 = TextNode("Placeholder", TextType.TEXT)
+        node2 = TextNode("placeholder", TextType.TEXT)
+        self.assertNotEqual(node1, node2)
+
+    def test_eq_2(self):
+        node = TextNode("This is a text node", TextType.CODE, None)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertEqual(node, node2)
+    
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_italic(self):
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic")
+    
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_link(self):
+        node = TextNode("Click on this link", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click on this link")
+        self.assertEqual(
+            html_node.props,
+            {"href": "https://www.boot.dev"},
+        )
+
+if __name__ == "__main__":
+    unittest.main()
